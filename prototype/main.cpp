cfd5f8f7ad12f8e4bc5fbc33f8b14b97e5dd66f8
Moonki3 <sepetyi.oleh@gmail.com>
2020-11-15 17:16:33 +0200

    Слегонца так сказать
    пойду кушац и еще поделаю

--- prototype/main.cpp
+++ prototype/main.cpp
@@ -1,11 +1,20 @@
 #include "mainwindow.h"
-
+#include <QFile>
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    QFile styleF;
+
+    styleF.setFileName(":/new/darkstyle.css");
+    styleF.open(QFile::ReadOnly);
+    QString cssStr = styleF.readAll();
+
+    qApp->setStyleSheet(cssStr);
+
+
     w.show();
     return a.exec();
 }
