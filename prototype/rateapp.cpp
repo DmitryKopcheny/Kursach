099d885afb6d0adaba016d292be1f35a93c2d6e8
Moonki3 <sepetyi.oleh@gmail.com>
2020-11-16 19:59:03 +0200

    rateApp

--- prototype/rateapp.cpp
+++ prototype/rateapp.cpp
@@ -6,9 +6,41 @@ rateApp::rateApp(QWidget *parent) :
     ui(new Ui::rateApp)
 {
     ui->setupUi(this);
+    ui->label_2->hide();
 }
 
 rateApp::~rateApp()
 {
     delete ui;
+}
+
+void rateApp::on_rate1_clicked()
+{
+    ui->label_2->move(95,150);
+    ui->label_2->show();
+
+}
+
+void rateApp::on_rate2_clicked()
+{
+    ui->label_2->move(155,150);
+    ui->label_2->show();
+}
+
+void rateApp::on_rate3_clicked()
+{
+    ui->label_2->move(215,150);
+    ui->label_2->show();
+}
+
+void rateApp::on_rate4_clicked()
+{
+    ui->label_2->move(275,150);
+    ui->label_2->show();
+}
+
+void rateApp::on_rate5_clicked()
+{
+    ui->label_2->move(335,150);
+    ui->label_2->show();
 }
